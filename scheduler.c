6d0cfa4a34ac5235d355558a7783469d52635cdd
James Miller <jam1211miller@gmail.com>
2023-04-02 15:12:14 -0700

    Updated code

--- scheduler.c
+++ scheduler.c
@@ -1,3 +1,8 @@
+// James Miller
+// CST - 315
+// 04/02/2023
+// Dr. Citro
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -5,7 +10,6 @@
 #include <unistd.h>
 #include <time.h>
 
-// Created enum to define each state.
 typedef enum
 {
     RUN,
@@ -13,158 +17,200 @@ typedef enum
     READY
 } State;
 
-// Basic Node structure
-typedef struct Node
+typedef struct Process
 {
-    int jobId;
-    int processingTime;
+    int processId;
+    int burstTime;
     State state;
-    struct Node *next;
-} Node;
+    struct Process *next;
+} Process;
 
-// Linked List structure
-typedef struct LinkedList
+typedef struct ProcessList
 {
-    Node *head;
+    Process *head;
     int size;
-} LinkedList;
+} ProcessList;
 
-// Linked list
-void initLinkedList(LinkedList *list)
+// Function prototypes
+void createProcessList(ProcessList *list);
+Process *createProcess(int processId, int burstTime);
+void addProcess(ProcessList *list, int processId, int burstTime);
+void simulateIO(ProcessList *list, int processId, bool ioComplete);
+bool isIOCompleted(Process *process);
+void *processScheduler(void *arg);
+void printProcessQueue(ProcessList *list);
+
+void createProcessList(ProcessList *list)
 {
     list->head = NULL;
     list->size = 0;
 }
 
-// Create a new node
-Node *createNode(int jobId, int processingTime)
+Process *createProcess(int processId, int burstTime)
 {
-    Node *newNode = (Node *)malloc(sizeof(Node));
-    newNode->jobId = jobId;
-    newNode->processingTime = processingTime;
-    newNode->state = RUN;
-    newNode->next = NULL;
-    return newNode;
+    Process *newProcess = (Process *)malloc(sizeof(Process));
+    newProcess->processId = processId;
+    newProcess->burstTime = burstTime;
+    newProcess->state = READY;
+    newProcess->next = NULL;
+    return newProcess;
 }
 
-// Add a new node to the end of the list
-void addNode(LinkedList *list, int jobId, int processingTime)
+void addProcess(ProcessList *list, int processId, int burstTime)
 {
-    Node *newNode = createNode(jobId, processingTime);
+    Process *newProcess = createProcess(processId, burstTime);
 
     if (list->head == NULL)
     {
-        list->head = newNode;
+        list->head = newProcess;
     }
     else
     {
-        Node *current = list->head;
-        while (current->next != NULL)
+        Process *current = list->head;
+        Process *prev = NULL;
+        while (current != NULL && current->burstTime < newProcess->burstTime)
         {
+            prev = current;
             current = current->next;
         }
-        current->next = newNode;
+        if (prev == NULL)
+        {
+            newProcess->next = list->head;
+            list->head = newProcess;
+        }
+        else
+        {
+            newProcess->next = current;
+            prev->next = newProcess;
+        }
     }
-
     list->size++;
 }
 
-void *processScheduler(void *arg)
+void simulateIO(ProcessList *list, int processId, bool ioComplete)
 {
-    LinkedList *list = (LinkedList *)arg;
-
-    while (true)
-    {
-        Node *current = list->head;
-        while (current != NULL)
-        {
-            if (current->state == WAIT && current->processingTime <= 0)
-            {
-                current->state = READY;
-                // Condition to change state from WAIT to RUN
-                if (rand() % 2 == 0)
-                {
-                    current->state = RUN;
-                }
-            }
-            else if (current->state == RUN && current->processingTime <= 0)
-            {
-                current->state = READY;
-            }
-
-            current = current->next;
-        }
-        sleep(1);
-    }
-
-    return NULL;
-}
-
-void simulateIO(LinkedList *list, int jobId)
-{
-    Node *current = list->head;
+    Process *current = list->head;
     while (current != NULL)
     {
-        if (current->jobId == jobId)
+        if (current->processId == processId)
         {
-            current->state = WAIT;
+            current->state = ioComplete ? READY : WAIT;
             break;
         }
         current = current->next;
     }
 }
 
-void fcfs(LinkedList *list)
+bool isIOCompleted(Process *process)
 {
-    int waitingTime = 0;
-    int turnaroundTime = 0;
+    // Simulate I/O completion for demonstration purposes.
+    return rand() % 2 == 0;
+}
 
-    printf("JobID  Waiting Time  Turnaround Time\n");
-    printf("------------------------------------\n");
-
-    Node *current = list->head;
+void printProcessQueue(ProcessList *list)
+{
+    Process *current = list->head;
+    printf("Current Process Queue:\n");
+    printf("----------------------\n");
+    printf("ProcessID  Burst Time  State\n");
     while (current != NULL)
     {
-        if (current->state == RUN)
+        printf("%-9d  %-10d  ", current->processId, current->burstTime);
+        switch (current->state)
         {
-            turnaroundTime = waitingTime + current->processingTime;
-
-            printf("%-5d  %-12d  %-15d\n", current->jobId, waitingTime, turnaroundTime);
-
-            waitingTime = turnaroundTime;
-            current->processingTime = 0;
+        case RUN:
+            printf("RUN\n");
+            break;
+        case WAIT:
+            printf("WAIT\n");
+            break;
+        case READY:
+            printf("READY\n");
+            break;
         }
         current = current->next;
     }
+    printf("\n");
+}
+
+void *processScheduler(void *arg)
+{
+    ProcessList *list = (ProcessList *)arg;
+    int waitingTime = 0;
+    int turnaroundTime = 0;
+    int totalWaitingTime = 0;
+    int executedProcesses = 0;
+
+    printf("ProcessID  Waiting Time  Turnaround Time\n");
+    printf("----------------------------------------\n");
+
+    while (true)
+    {
+        printProcessQueue(list);
+
+        Process *current = list->head;
+        while (current != NULL)
+        {
+            if (current->state == READY)
+            {
+                printf("Executing ProcessID %d\n", current->processId);
+                turnaroundTime = waitingTime + current->burstTime;
+                printf("%-9d  %-12d  %-15d\n", current->processId, waitingTime, turnaroundTime);
+
+                totalWaitingTime += waitingTime;
+                executedProcesses++;
+                waitingTime = turnaroundTime;
+                current->burstTime = 0;
+                current->state = RUN;
+            }
+            else if (current->state == WAIT && isIOCompleted(current))
+            {
+                current->state = READY;
+            }
+            current = current->next;
+        }
+
+        if (executedProcesses > 0)
+        {
+            float avgWaitingTime = (float)totalWaitingTime / executedProcesses;
+            printf("Average Waiting Time: %.2f\n", avgWaitingTime);
+        }
+
+        sleep(1);
+    }
+
+    return NULL;
 }
 
 int main()
 {
     srand(time(NULL));
-    LinkedList list;
-    initLinkedList(&list);
 
-    addNode(&list, 1, 5);
-    addNode(&list, 2, 8);
-    addNode(&list, 3, 12);
+    ProcessList list;
+    createProcessList(&list);
+
+    addProcess(&list, 1, 5);
+    addProcess(&list, 2, 8);
+    addProcess(&list, 3, 12);
 
     pthread_t schedulerThread;
     pthread_create(&schedulerThread, NULL, processScheduler, &list);
 
-    // Simulate I/O request for JobID 2
+    // Simulate I/O request for ProcessID 2
     sleep(2);
-    simulateIO(&list, 2);
+    simulateIO(&list, 2, false);
 
-    sleep(5);
+    // Simulate I/O completion for ProcessID 2
+    sleep(4);
+    simulateIO(&list, 2, true);
 
-    fcfs(&list);
-
+    sleep(10);
     pthread_cancel(schedulerThread);
     pthread_join(schedulerThread, NULL);
 
-    // Free the memory allocated for the nodes
-    Node *current = list.head;
-    Node *temp;
+    // Free the memory allocated for the processes
+    Process *current = list.head;
+    Process *temp;
     while (current != NULL)
     {
         temp = current;
